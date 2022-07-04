1e9356e0eaf6f2d8759df2315a7c9edc07351025
Maksimum Farta <maksim-isaev1989@yandex.ru>
2022-07-04 23:58:52 +0300

    добавлено создание папок для общей формы

--- create_folders.py
+++ create_folders.py
@@ -1,32 +1,44 @@
 import os
+
+folder = 56
+if folder == 56:
+    folder_list = ['Сведения_отдЭКМПиМЭ', 'Кадры_отдЭКМПиМЭ', 'Деятельность_отдЭКМПиМЭ', 'Выезды_отдЭКМПиМЭ']
+    form = r'/ФОРМА 56'
+    papka_files1 = 'для_программы'
+    papka_files2 = 'переработанные_файлы'
+if folder == 55:
+    folder_list = [r'/Табл_сведения_о_центре_МК', r'/Сведения_о_кадрах_мк', r'/Формирования_мк',
+                   r'/Сведения_о_пострадавших_ЧС', r'/Сведения_о_видах_помощи_вЧС', r'/Использование_КФ_приЧС',
+                   r'/Сведения_о_лаборатории_мк', r'/Сведения_о_обучении', r'/Сведения_о_учениях_трениров_занят',
+                   r'/Сведения_о_трассовых_пунктах', r'/Сведения_о_МТО_МК']
+    form = r'/ФОРМА 55'
+    papka_files1 = 'для программы'
+    papka_files2 = 'переработанные файлы'
+else:
+    pass
 base_path = r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)'
-papki = os.listdir(base_path)
+
+
 # создадим папки для работы
-for x, y in enumerate(papki):
-    os.chdir(base_path + f'/{y}')
-    if 'ФОРМА 55' in os.listdir():
-        os.chdir(os.getcwd() + r'/ФОРМА 55')
-    if 'для программы' in os.listdir():
-        os.chdir(os.getcwd() + r'/для программы')
-    else:
-        os.mkdir(os.getcwd() + r'/для программы')
-        os.chdir(os.getcwd() + r'/для программы')
-    if 'переработанные файлы' in os.listdir():
-        os.chdir(os.getcwd() + r'/переработанные файлы')
-    else:
-        os.mkdir(os.getcwd() + r'/переработанные файлы')
-        os.chdir(os.getcwd() + r'/переработанные файлы')
-    if 'Табл_сведения_о_центре_МК1' in os.listdir():
-        os.chdir(os.getcwd() + r'/Табл_сведения_о_центре_МК')
-    else:
-        os.mkdir(os.getcwd() + r'/Табл_сведения_о_центре_МК')
-        os.mkdir(os.getcwd() + r'/Сведения_о_кадрах_мк')
-        os.mkdir(os.getcwd() + r'/Формирования_мк')
-        os.mkdir(os.getcwd() + r'/Сведения_о_пострадавших_ЧС')
-        os.mkdir(os.getcwd() + r'/Сведения_о_видах_помощи_вЧС')
-        os.mkdir(os.getcwd() + r'/Использование_КФ_приЧС')
-        os.mkdir(os.getcwd() + r'/Сведения_о_лаборатории_мк')
-        os.mkdir(os.getcwd() + r'/Сведения_о_обучении')
-        os.mkdir(os.getcwd() + r'/Сведения_о_учениях_трениров_занят')
-        os.mkdir(os.getcwd() + r'/Сведения_о_трассовых_пунктах')
-        os.mkdir(os.getcwd() + r'/Сведения_о_МТО_МК')
+def create_folder(base_path, form, papka_files1, papka_files2, folder_list):
+    papki = os.listdir(base_path)
+    for x, y in enumerate(papki):
+        os.chdir(base_path + f'/{y}' + form)
+        if form in os.listdir():
+            os.chdir(os.getcwd() + form)
+        if papka_files1 in os.listdir():
+            os.chdir(os.getcwd() + f'/{papka_files1}')
+        else:
+            os.mkdir(os.getcwd() + f'/{papka_files1}')
+            os.chdir(os.getcwd() + f'/{papka_files1}')
+        if papka_files2 in os.listdir():
+            os.chdir(os.getcwd() + f'/{papka_files2}')
+        else:
+            os.mkdir(os.getcwd() + f'/{papka_files2}')
+            os.chdir(os.getcwd() + f'/{papka_files2}')
+        for i, j in enumerate(folder_list):
+            os.mkdir(os.getcwd() + f'/{j}')
+
+
+create_folder(base_path=base_path, form=form, papka_files1=papka_files1,
+              papka_files2=papka_files2, folder_list=folder_list)
---
+++ form56_clear.py
@@ -0,0 +1,6 @@
+##
+import os
+base_path = r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)'
+papki = os.listdir(base_path)
+print(papki)
+##
