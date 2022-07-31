0b051b9e3024446d3c3ff99f85102de13a82dbd3
Maksimum Farta <maksim-isaev1989@yandex.ru>
2022-07-31 22:31:20 +0300

    в разработке находится функция по группировке данных по папкам

--- form56_clear.py
+++ form56_clear.py
@@ -1,12 +1,29 @@
 ##
 import os
+from rename_files import rename_region, fly_yes_llc_group, fly_no_llc_group, no_fly_group
+from osnova_clear import choise_paths
 import pandas as pd
 import re
 import numpy as np
 from other_function import drop_num, drop_num_2
-
-base_path = r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2015 ' \
-            r'г/ФОРМА 55/для программы'
-papki = [x for x in os.listdir(base_path) if x.endswith(('.xls', '.xlsx'))]
+forma = 55
+if forma == 55:
+    base_path = choise_paths(forma=55)
+    os.chdir(base_path[0] + r'/переработанные файлы')
+    for folder in os.listdir():
+        folder_path = os.getcwd()
+        os.chdir(folder_path + f'/{folder}')
+        dirs = [x for x in os.listdir() if x.endswith(('.xls', '.xlsx'))]
+        os.chdir(folder_path + f'/{folder}')
+if forma == 56:
+    base_path = choise_paths(forma=56)
+    os.chdir(base_path[0] + r'/переработанные_файлы')
+    for folder in os.listdir():
+        folder_path = os.getcwd()
+        os.chdir(folder_path + f'/{folder}')
+        dirs = [x for x in os.listdir() if x.endswith(('.xls', '.xlsx'))]
+        os.chdir(folder_path + f'/{folder}')
+##
+print(dirs)
 
 
