881e713f1af89028e15571c442980d72cf5076e2
Maksimum Farta <maksim-isaev1989@yandex.ru>
2022-07-31 20:18:46 +0300

    сделан консольный интерфейс для выбора режима работы

--- create_folders.py
+++ create_folders.py
@@ -1,24 +1,6 @@
 import os
 import shutil
 
-folder = 55
-if folder == 56:
-    folder_list = ['Сведения_отдЭКМПиМЭ', 'Кадры_отдЭКМПиМЭ', 'Деятельность_отдЭКМПиМЭ', 'Выезды_отдЭКМПиМЭ']
-    form = r'/ФОРМА 56'
-    papka_files1 = 'для_программы'
-    papka_files2 = 'переработанные_файлы'
-if folder == 55:
-    folder_list = [r'/Табл_сведения_о_центре_МК', r'/Сведения_о_кадрах_мк', r'/Формирования_мк',
-                   r'/Сведения_о_пострадавших_ЧС', r'/Сведения_о_видах_помощи_вЧС', r'/Использование_КФ_приЧС',
-                   r'/Сведения_о_лаборатории_мк', r'/Сведения_о_обучении', r'/Сведения_о_учениях_трениров_занят',
-                   r'/Сведения_о_трассовых_пунктах', r'/Сведения_о_МТО_МК']
-    form = r'/ФОРМА 55'
-    papka_files1 = 'для программы'
-    papka_files2 = 'переработанные файлы'
-else:
-    pass
-base_path = r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)'
-
 
 # создадим папки для работы
 def create_folder(base_path, form, papka_files1, papka_files2, folder_list):
@@ -39,9 +21,21 @@ def create_folder(base_path, form, papka_files1, papka_files2, folder_list):
                 os.mkdir(os.getcwd() + f'/{papka_files2}')
                 os.chdir(os.getcwd() + f'/{papka_files2}')
             for i, j in enumerate(folder_list):
-                os.mkdir(os.getcwd() + f'/{j}')
-
-
+                if j[1:] in os.listdir(os.getcwd()):
+                    os.chdir(os.getcwd() + f'{j}')
+                    group_path = os.getcwd()
+                    os.mkdir(group_path + r'/группа_1')
+                    os.mkdir(group_path + r'/группа_2')
+                    os.mkdir(group_path + r'/группа_3')
+                    os.chdir(base_path + f'/{y}' + form + f'/{papka_files1}' + f'/{papka_files2}')
+                else:
+                    os.mkdir(os.getcwd() + f'{j}')
+                    os.chdir(os.getcwd() + f'{j}')
+                    group_path = os.getcwd()
+                    os.mkdir(group_path + r'/группа_1')
+                    os.mkdir(group_path + r'/группа_2')
+                    os.mkdir(group_path + r'/группа_3')
+                    os.chdir(base_path + f'/{y}' + form + f'/{papka_files1}' + f'/{papka_files2}')
 
 
 def del_folder(base_path, form, papka_files1, papka_files2):
@@ -49,7 +43,7 @@ def del_folder(base_path, form, papka_files1, papka_files2):
     for x, y in enumerate(papki):
         if os.path.isdir(base_path + f'/{y}'):
             os.chdir(base_path + f'/{y}' + form)
-            if form in os.listdir():
+            if form[:1] in os.listdir():
                 os.chdir(os.getcwd() + form)
             if papka_files1 in os.listdir():
                 os.chdir(os.getcwd() + f'/{papka_files1}')
@@ -60,18 +54,17 @@ def del_folder(base_path, form, papka_files1, papka_files2):
             else:
                 continue
 
-def create_or_del():
-    while True:
-        choise = int(input('Введите 1 если хотите создать папки для файлов, или 2 если хотите удалить папки '
-                           'с файлами '))
-        if choise == 1:
-            create_folder(base_path=base_path, form=form, papka_files1=papka_files1,
-                          papka_files2=papka_files2, folder_list=folder_list)
-            break
-        elif choise == 2:
-            del_folder(base_path=base_path, form=form, papka_files1=papka_files1, papka_files2=papka_files2)
-            break
-        else:
-            print('Вы не выбрали действие')
 
-create_or_del()
+# def create_or_del():
+#     while True:
+#         choise = int(input('Введите 1 если хотите создать папки для файлов, или 2 если хотите удалить папки '
+#                            'с файлами '))
+#         if choise == 1:
+#             create_folder(base_path=base_path, form=form, papka_files1=papka_files1,
+#                           papka_files2=papka_files2, folder_list=folder_list)
+#             break
+#         elif choise == 2:
+#             del_folder(base_path=base_path, form=form, papka_files1=papka_files1, papka_files2=papka_files2)
+#             break
+#         else:
+#             print('Вы не выбрали действие')
--- osnova_clear.py
+++ osnova_clear.py
@@ -1,55 +1,132 @@
-##
 import os
 import re
 import warnings
+from create_folders import del_folder, create_folder
+from other_function import clear_df
 
 warnings.filterwarnings('ignore')
 DEBUG = True
-from other_function import clear_df
 
-forma = 56
-if forma == 55:
-    base_path = [r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2015 '
-                 r'г/ФОРМА 55/для программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2016 '
-                 r'г/ФОРМА 55/для программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2017 '
-                 r'г/ФОРМА 55/для программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
-                 r'(2015-2020гг)/2018 г/ФОРМА 55/для программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
-                 r'(2015-2020гг)/2019 г/ФОРМА 55/для программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
-                 r'(2015-2020гг)/2020 г/ФОРМА 55/для программы']
-if forma == 56:
-    base_path = [r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2015'
-                 r' г/ФОРМА 56/для_программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2016 '
-                 r'г/ФОРМА 56/для_программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2017 '
-                 r'г/ФОРМА 56/для_программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
-                 r'(2015-2020гг)/2018 г/ФОРМА 56/для_программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
-                 r'(2015-2020гг)/2019 г/ФОРМА 56/для_программы',
-                 r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
-                 r'(2015-2020гг)/2020 г/ФОРМА 56/для_программы']
-print(base_path)
-##
-problems = {}
-for x in base_path:
-    c = ''.join(re.findall(r'\d+', x.split(r'/')[-3]))
-    problems[c] = clear_df(base_path=x, forma=forma, problems=problems)
-if forma == 55:
-    os.chdir(r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)')
-    with open('problems_form55.txt', 'w') as f:
-        for key, val in problems.items():
-            f.write(f'{key}: {val}\n')
-if forma == 56:
-    os.chdir(r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)')
-    with open('problems_form56.txt', 'w') as f:
-        for key, val in problems.items():
-            f.write(f'{key}: {val}\n')
-##
-print(problems.keys())
-##
+
+def choise_work():
+    while True:
+        pr_str_1 = 'Введите 1 если хотите обновить выгрузку данных 55 формы'
+        pr_str_2 = 'Введите 2 если хотите обновить выгрузку данных 56 формы'
+        pr_str_3 = 'Введите 3 если хотите удалить файлы 55 формы'
+        pr_str_4 = 'Введите 4 если хотите удалить файлы 56 формы'
+        pr_str_5 = 'Введите 5 если хотите создать папки для 55 формы'
+        pr_str_6 = 'Введите 6 если хотите создать папки для 56 формы'
+        pr_str_7 = 'Введите 7 если хотите выйти из программы'
+        print('-' * 60)
+        print(f'{pr_str_1:>60}')
+        print(f'{pr_str_2:>60}')
+        print(f'{pr_str_3:>60}')
+        print(f'{pr_str_4:>60}')
+        print(f'{pr_str_5:>60}')
+        print(f'{pr_str_6:>60}')
+        print(f'{pr_str_7:>60}')
+        print('-' * 60)
+        choise = int(input())
+        if choise == 1:
+            forma = 55
+            base_path = choise_paths(forma=forma)
+            problems = {}
+            for path in base_path:
+                year = ''.join(re.findall(r'\d+', path.split(r'/')[-3]))
+                problems[year] = clear_df(base_path=path, forma=forma, problems=problems)
+            os.chdir(r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)')
+            with open('problems_form55.txt', 'w') as f:
+                for key, val in problems.items():
+                    f.write(f'{key}: {val}\n')
+            print(problems)
+            break
+        elif choise == 2:
+            forma = 56
+            base_path = choise_paths(forma=forma)
+            problems = {}
+            for path in base_path:
+                year = ''.join(re.findall(r'\d+', path.split(r'/')[-3]))
+                problems[year] = clear_df(base_path=path, forma=forma, problems=problems)
+            os.chdir(r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)')
+            with open('problems_form56.txt', 'w') as f:
+                for year, problem in problems.items():
+                    f.write(f'{year}: {problem}\n')
+            print(problems)
+            break
+        elif choise == 3:
+            forma = r'/ФОРМА 55'
+            papka_files1 = 'для программы'
+            papka_files2 = 'переработанные файлы'
+            base_path = r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)'
+            del_folder(base_path=base_path, form=forma, papka_files1=papka_files1,
+                       papka_files2=papka_files2)
+            break
+        elif choise == 4:
+            forma = r'/ФОРМА 56'
+            papka_files1 = 'для_программы'
+            papka_files2 = 'переработанные_файлы'
+            base_path = r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)'
+            del_folder(base_path=base_path, form=forma, papka_files1=papka_files1,
+                       papka_files2=papka_files2)
+            break
+        elif choise == 5:
+            base_path = '/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)'
+            folder_list = [r'/Табл_сведения_о_центре_МК', r'/Сведения_о_кадрах_мк', r'/Формирования_мк',
+                           r'/Сведения_о_пострадавших_ЧС', r'/Сведения_о_видах_помощи_вЧС', r'/Использование_КФ_приЧС',
+                           r'/Сведения_о_лаборатории_мк', r'/Сведения_о_обучении',
+                           r'/Сведения_о_учениях_трениров_занят',
+                           r'/Сведения_о_трассовых_пунктах', r'/Сведения_о_МТО_МК']
+            form = r'/ФОРМА 55'
+            papka_files1 = 'для программы'
+            papka_files2 = 'переработанные файлы'
+            create_folder(base_path=base_path, folder_list=folder_list, form=form, papka_files1=papka_files1,
+                          papka_files2=papka_files2)
+            break
+        elif choise == 6:
+            base_path = '/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)'
+            folder_list = [r'/Сведения_отдЭКМПиМЭ', r'/Кадры_отдЭКМПиМЭ',
+                           r'/Деятельность_отдЭКМПиМЭ', r'/Выезды_отдЭКМПиМЭ']
+            form = r'/ФОРМА 56'
+            papka_files1 = 'для_программы'
+            papka_files2 = 'переработанные_файлы'
+            create_folder(base_path=base_path, folder_list=folder_list, form=form, papka_files1=papka_files1,
+                          papka_files2=papka_files2)
+            break
+        elif choise == 7:
+            break
+        else:
+            print('Вы не выбрали действие!')
+
+
+def choise_paths(forma):
+    if forma == 55:
+        base_path = [r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2015 '
+                     r'г/ФОРМА 55/для программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2016 '
+                     r'г/ФОРМА 55/для программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2017 '
+                     r'г/ФОРМА 55/для программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
+                     r'(2015-2020гг)/2018 г/ФОРМА 55/для программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
+                     r'(2015-2020гг)/2019 г/ФОРМА 55/для программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
+                     r'(2015-2020гг)/2020 г/ФОРМА 55/для программы']
+        return base_path
+    if forma == 56:
+        base_path = [r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2015'
+                     r' г/ФОРМА 56/для_программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2016 '
+                     r'г/ФОРМА 56/для_программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 (2015-2020гг)/2017 '
+                     r'г/ФОРМА 56/для_программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
+                     r'(2015-2020гг)/2018 г/ФОРМА 56/для_программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
+                     r'(2015-2020гг)/2019 г/ФОРМА 56/для_программы',
+                     r'/media/maks/ntfs/Ирина/диссер статистика/Ф.55,56 (2015-2020гг)/Ф.55,56 '
+                     r'(2015-2020гг)/2020 г/ФОРМА 56/для_программы']
+        return base_path
+
+
+choise_work()
